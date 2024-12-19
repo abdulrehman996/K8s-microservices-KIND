497fbbd6b10053dc9fa23b576bfcd5424c467ea1
Abdul Rehman <58827592+abdulrehman996@users.noreply.github.com>
2024-12-19 15:03:28 +0500

    Delete backend directory

--- backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import SubmitDataView, DisplayDataView
-
-urlpatterns = [
-    path('submit/', SubmitDataView.as_view()),
-    path('display/', DisplayDataView.as_view()),
-] 
